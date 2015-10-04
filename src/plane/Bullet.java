2fcb1cac219f426ce2bc7f985c132860bc860679
change <844631704s@gmail.com>
2015-10-04 20:45:43 +0800

    add bullet

---
+++ src/plane/Bullet.java
@@ -0,0 +1,35 @@
+package plane;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import util.Constant;
+
+public class Bullet {
+	double x,y;
+	int speed=3;
+	double degree;
+	int width=10;int height=10;
+	public Bullet(){
+		degree=Math.random()*Math.PI*2;
+		x = Constant.GAME_WIDTH/2;
+		y = Constant.GAME_WIDTH/2;
+	}
+	public void draw(Graphics g){
+		Color c=g.getColor();
+		g.setColor(Color.yellow);
+		g.fillOval((int)x, (int)y, width, height);
+		System.out.println("1");
+		//move bullet
+		x+=speed*Math.cos(degree);
+		y+=speed*Math.sin(degree);
+		if(y>Constant.GAME_HEIGHT-height||y<30){
+			degree = -degree;
+		}
+		
+		if(x<0||x>Constant.GAME_WIDTH-width){
+			degree = Math.PI-degree;
+		}
+		g.setColor(c);
+	}
+}
--- src/plane/PlaneFrame.java
+++ src/plane/PlaneFrame.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import util.Constant;
 import util.GameUtil;
@@ -12,15 +13,23 @@ import util.MyFrame;
 public class PlaneFrame extends MyFrame{
 	Image bg=GameUtil.getImage("images/bg.jpg");
 	Plane p=new Plane("images/plane.png",Constant.PLANE_X,Constant.PLANE_Y);
-	
+	ArrayList bulletlist=new ArrayList();
 	public void paint(Graphics g){
 		g.drawImage(bg, 0, 0, null);
 		p.draw(g);
+		for(int i=0;i<bulletlist.size();i++){
+			Bullet b=(Bullet)bulletlist.get(i);
+			b.draw(g);
+		}
 	}
 	@Override
 	public void lauchFrame(){
 		super.lauchFrame();
 		addKeyListener(new KeyMonitor());
+		for(int i=0;i<50;i++){
+			Bullet b=new Bullet();
+			bulletlist.add(b);
+		}
 	}
 	//定义为内部类，可以方便的使用外部类的普通属性
 	class KeyMonitor extends KeyAdapter{
@@ -28,7 +37,6 @@ public class PlaneFrame extends MyFrame{
 		@Override
 		public void keyPressed(KeyEvent e) {
 			super.keyPressed(e);
-			System.out.println(e.getKeyCode());
 			p.direction(e.getKeyCode(),true);
 		}
 
